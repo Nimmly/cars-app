8fa28a271d1683bb457ee86d1cf30504dee63b33
Nimmly <marko.v.popovic@gmail.comgot config --global user.name Nimmlygit config --global user.email marko.v.popovic@gmail.com>
2018-05-08 18:40:10 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <AppCars />
+  </div>
+</template>
+
+<script>
+import AppCars from './components/AppCars.vue'
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    AppCars,
+    NavBar
+  }
+}
+</script>
+
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <router-link class='nav-link' to="/cars">Cars </router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
